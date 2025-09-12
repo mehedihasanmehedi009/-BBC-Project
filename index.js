27068cdd67fa46f73983c0d766ac8822c48c4357
mehedihasanmehedi009 <mehedihasanmehedi007008@gmail.com>
2025-09-12 23:49:52 +0600

    mehedi

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const CategoriesContainer = document.getElementById("Categories-Container");
+const NewsContainer = document.getElementById("news-container");
 
+// heder function
 const LoadCategories = () => {
   fetch("https://news-api-fs.vercel.app/api/categories")
     .then((res) => res.json())
@@ -9,15 +11,44 @@ const Display = (mun) => {
   mun.forEach((cat) => {
     // console.log(cat);
     CategoriesContainer.innerHTML += `
-    <li id="${cat.id}" class="hover:border-b-4 border-red-500 cursor-pointer">
+    <li id="${cat.id}"  class="border-red-500  hover:border-red-500 hover:border-b-4  cursor-pointer">
             ${cat.title}
           </li>
     `;
   });
   CategoriesContainer.addEventListener("click", (e) => {
-    if (e.target.locaName === "li") console.log(e.target);
-    e.target.classList.add("border-b-4", "border-red-500");
+    const allLi = document.querySelectorAll("li");
+    allLi.forEach((li) => li.classList.remove("border-b-4"));
+    if (e.target.localName === "li") e.target.classList.add("border-b-4");
+    LoadNewsCategories(e.target.id);
+  });
+};
+
+// img function and
+const LoadNewsCategories = (categoryId) => {
+  // console.log(categoryId);
+  fetch(`https://news-api-fs.vercel.app/api/categories/${categoryId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      showNewsByCategorys(data.articles);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+const showNewsByCategorys = (articles) => {
+  NewsContainer.innerHTML = "";
+  articles.forEach((article) => {
+    NewsContainer.innerHTML += `
+   <div >
+    <div>
+  <img src="${article.image.srcset[5].url}" alt="" />
+</div>
+<h1>${article.title}</h1>
+<p>${article.time}</p>
+   </div>`;
   });
 };
 
 LoadCategories();
+LoadNewsCategories("main");
